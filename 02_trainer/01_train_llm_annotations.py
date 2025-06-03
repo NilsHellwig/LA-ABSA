d59988fb8ff7bd4dbdf98cd0779d82efc95c2a4b
NilsHellwig <nc.hellwig@gmail.com>
2025-06-03 05:43:18 +0000

    add llm_annotate_train llama

--- 02_trainer/01_train_llm_annotations.py
+++ 02_trainer/01_train_llm_annotations.py
@@ -1,7 +1,7 @@
 import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../zero-shot-absa-quad/')))
 from dataloader import DataLoader
-from trainer import train_paraphrase, train_mvp, train_dlo
+from trainer import train_paraphrase, train_mvp, train_dlo, train_llm
 import json
 import time
 
@@ -9,18 +9,21 @@ from helper import clean_up, create_output_directory
 
 dataloader = DataLoader("../zero-shot-absa-quad/datasets", "../zero-shot-absa-quad/fs_examples")
 
-for ml_method in ["dlo", "paraphrase"]:
+model_name_or_path = "Meta-Llama-3.1-8B-Instruct-bnb-4bit"
+
+for ml_method in [f"llm_{model_name_or_path}", "dlo", "paraphrase"]:
  for seed in range(5):
    for ds_name in ["rest16", "hotels", "rest15", "flightabsa", "coursera"]:
      for fs_num in [50, 10, 0]:
        for task in ["tasd", "asqp"]:
-         for n_llm_examples in [800, "full"]:  
+         for n_llm_examples in ["full"]:  
             train_ds = dataloader.load_data(ds_name, "train", cv=False, target=task, fs_num=fs_num, fs_ann_mode=True, n_ann_examples=n_llm_examples)
             test_ds = dataloader.load_data(ds_name, "test", cv=False, target=task)
       
          
             print(f"Task:", task, "Dataset:", ds_name, "Seed:", seed, "ML-Method:", ml_method, "FS-Num:", fs_num, "len(train_ds)", len(train_ds), "len(test_ds)", len(test_ds), "n_llm_examples", n_llm_examples)
-            filename = f"./_out_fine_tunings/01_llm_annotate_train/{ml_method}_{n_llm_examples}_{task}_{fs_num}_{ds_name}_{seed}.json"
+            cond_name = f"{ml_method}_{n_llm_examples}_{task}_{fs_num}_{ds_name}_{seed}"
+            filename = f"./_out_fine_tunings/01_llm_annotate_train/{cond_name}.json"
 
             if os.path.exists(filename):
                print(f"File {filename} already exists. Skipping.")
@@ -29,6 +32,23 @@ for ml_method in ["dlo", "paraphrase"]:
             
                clean_up()
                create_output_directory()
+               
+               if ml_method == f"llm_{model_name_or_path}":
+                  unique_aspect_categories = sorted(
+                   {
+                      aspect["aspect_category"]
+                      for entry in dataloader.load_data(
+                          name=ds_name, data_type="all", target=task
+                      )
+                      for aspect in entry["aspects"]
+                   }
+                  )
+                  scores = train_llm(train_ds=train_ds, test_ds=test_ds, seed=seed, dataset=ds_name, task=task, model_name_or_path=model_name_or_path, cond_name=cond_name, unique_aspect_categories=unique_aspect_categories)
+                  clean_up(output_directory=f'./{cond_name}')
+               
+                  # remove cache_res.json
+                  if os.path.exists(f"{cond_name}_res.json"):
+                      os.remove(f"{cond_name}_res.json")
               
                if ml_method == "paraphrase":
                   scores = train_paraphrase(train_ds=train_ds, test_ds=test_ds, seed=seed, dataset=ds_name, task=task)
