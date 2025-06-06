92406bb50515552523640956ba6bea0349dad686
NilsHellwig <nc.hellwig@gmail.com>
2025-06-06 05:47:23 +0000

    update time analysis

--- 02_trainer/01_train_llm_annotations_with_time.py
+++ 02_trainer/01_train_llm_annotations_with_time.py
@@ -1,7 +1,7 @@
 import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../zero-shot-absa-quad/')))
 from dataloader import DataLoader
-from trainer import train_paraphrase, train_mvp, train_dlo
+from trainer import train_paraphrase, train_mvp, train_dlo, train_llm
 import json
 import time
 
@@ -9,10 +9,11 @@ from helper import clean_up, create_output_directory
 
 dataloader = DataLoader("../zero-shot-absa-quad/datasets", "../zero-shot-absa-quad/fs_examples")
 
+model_name_or_path = "Meta-Llama-3.1-8B-Instruct-bnb-4bit"
 
 for seed in range(5):
    for ds_name in ["rest16", "hotels", "rest15", "flightabsa", "coursera"]:
-     for fs_num in [50, 10]:
+     for fs_num in [50, 10, 0]:
        for task in ["tasd", "asqp"]:
          for n_llm_examples in ["full"]:  
           for ml_method in ["dlo", "paraphrase"]:
@@ -21,7 +22,8 @@ for seed in range(5):
       
          
             print(f"Task:", task, "Dataset:", ds_name, "Seed:", seed, "ML-Method:", ml_method, "FS-Num:", fs_num, "len(train_ds)", len(train_ds), "len(test_ds)", len(test_ds), "n_llm_examples", n_llm_examples)
-            filename = f"./_out_fine_tunings/01_llm_annotate_train/{ml_method}_{n_llm_examples}_{task}_{fs_num}_{ds_name}_{seed}_PERFORMANCE.json"
+            cond_name = f"{ml_method}_{n_llm_examples}_{task}_{fs_num}_{ds_name}_{seed}_PERFORMANCE"
+            filename = f"./_out_fine_tunings/01_llm_annotate_train/{cond_name}.json"
 
             if os.path.exists(filename):
                print(f"File {filename} already exists. Skipping.")
@@ -31,6 +33,14 @@ for seed in range(5):
                clean_up()
                create_output_directory()
               
+               if ml_method == f"llm_{model_name_or_path}":
+                  train_ds = train_ds[:int(len(train_ds) / 2)]
+                  scores = train_llm(train_ds=train_ds, test_ds=test_ds, seed=seed, dataset=ds_name, task=task, model_name_or_path=model_name_or_path, cond_name=cond_name, num_train_epochs=1)
+                  clean_up(output_directory=f'./{cond_name}')
+                  
+                  if os.path.exists(f"{cond_name}_res.json"):
+                      os.remove(f"{cond_name}_res.json")
+                                        
                if ml_method == "paraphrase":
                   scores = train_paraphrase(train_ds=train_ds, test_ds=test_ds, seed=seed, dataset=ds_name, task=task, num_train_epochs=1)
                if ml_method == "mvp":
